f31a3b713d13c08384f002fbc85ac097ee8c6e5c
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-02-05 01:23:53 +0800

    [mabel] Add pokemon GET/:name route

--- src/routes/pokemons.route.js
+++ src/routes/pokemons.route.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const pokemons = [];
+const Pokemon = require("./models/pokemon.model");
+
 router.get("/", (req, res) => {
-  res.send(pokemons);
+  try {
+    const pokemons = await Pokemon.find(req.query);
+    res.send(pokemons);
+  } catch (err) {
+    next(err);
+  }
 });
+
+router.get("/:name", async (req, res, next) => {
+  try {
+    const name = req.params.name;
+    const regex = new RegExp(name, "gi");
+    const pokemons = await Pokemon.find({name: regex});
+    res.send(pokemons);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
