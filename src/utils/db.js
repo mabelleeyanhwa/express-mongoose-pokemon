38d59e3e1a8c38e2967c7a30e3e7f5561482a733
Mabel Lee <yanhwa.lee@thoughtworks.com>
2020-02-12 06:35:00 +0800

    [mabel] Change environment variable name for mongodb

--- src/utils/db.js
+++ src/utils/db.js
@@ -8,11 +8,11 @@ const mongoOptions = {
 };
 
 const dbName = "expressPokemon";
-const dbUrl = process.env.MONGO_URI || "mongodb://localhost:27017/" + dbName;
+const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/" + dbName;
 mongoose.connect(dbUrl, mongoOptions);
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("connected to mongodb");
+  console.log(`connected to mongodb, dbUrl: ${process.env.MONGODB_URI}`);
 });
